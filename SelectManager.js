aeaf51438deed17adf05e90d47f4a3044c0f8ca0
alex94539 <euphokumiko.iem06@nctu.edu.tw>
2022-08-02 00:01:53 +0800

    modified:   SelectManager.js 	modified:   SpineManager.js 	modified:   TrackManager.js 	modified:   main.html 	modified:   main.js

--- SelectManager.js
+++ SelectManager.js
@@ -24,17 +24,23 @@ class SelectManager {
 
         let thisSelectContainer = this._stMap.get(`selectFrame${nextLabel}`);
         thisSelectContainer.interactive = true;
+        const localBound = thisSelectContainer.getLocalBounds();
+        thisSelectContainer.pivot.set(localBound.width / 2, localBound.height / 2);
 
         thisSelectContainer.on('click', () => {
-            onClick(nextLabel);
-            afterSelection();
-            this._stMap.forEach(st => {
-                if (st.children.length) {
-                    st.removeChildren(0, st.children.length);
-                }
-            });
-            this._container.removeChildren(0, this._container.children.length);
-        });
+            this._disableInteractive();
+
+            TweenMax.to(thisSelectContainer, 0.3, { pixi: { scaleX: 1.1, scaleY: 1.1 } });
+
+            setTimeout(() => {
+
+                onClick(nextLabel);
+                afterSelection();
+
+                this._fadeOutOption();
+            }, 1000);
+
+        }, { once: true });
 
         let textObj = new PIXI.Text(selectDesc, {
             fontFamily: 'Meiryo',
@@ -51,15 +57,35 @@ class SelectManager {
 
         switch (nextLabel) {
             case "1":
-                thisSelectContainer.position.set(400, 40);
+                thisSelectContainer.position.set(568, 125);
                 break;
             case "2":
-                thisSelectContainer.position.set(60, 150);
+                thisSelectContainer.position.set(200, 240);
                 break;
             case "3":
-                thisSelectContainer.position.set(750, 150);
+                thisSelectContainer.position.set(936, 240);
                 break;
         }
 
+        const tl = new TimelineMax({ repeat: -1, yoyo: true, repeatDelay: 0 });
+        const yLocation = thisSelectContainer.y;
+        tl.to(thisSelectContainer, 1, { pixi: { y: yLocation - 10 }, ease: Power1.easeInOut });
+        tl.to(thisSelectContainer, 1, { pixi: { y: yLocation }, ease: Power1.easeInOut });
+
+    }
+
+    _disableInteractive() {
+        this._stMap.forEach(st => {
+            st.interactive = false;
+        });
+    }
+
+    _fadeOutOption() {
+        this._stMap.forEach(st => {
+            TweenMax.to(st, 1, { pixi: { alpha: 0 }, ease: Power1.easeInOut });
+        });
+        setTimeout(() => {
+            this._container.removeChildren(0, this._container.children.length);
+        });
     }
 }
