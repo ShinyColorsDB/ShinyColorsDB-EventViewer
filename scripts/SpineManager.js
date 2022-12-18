b6066e663bf3a5c15be76c122f4c6e7d40b6488a
Euphokumiko <h.k.h94539@gmail.com>
2022-12-18 20:50:08 +0800

    fix missing type

--- scripts/SpineManager.js
+++ scripts/SpineManager.js
@@ -14,7 +14,10 @@ class SpineManager {
             stand_costume_fix: 'stand_costume',
 
             stand_flex: 'stand',
-            stand_costume_flex: 'stand_costume'
+            stand_costume_flex: 'stand_costume',
+
+            stand: 'stand',
+            stand_costume: 'stand_costume'
         };
     }
 
