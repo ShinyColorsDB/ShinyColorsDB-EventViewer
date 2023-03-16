c739cd345bcb4b1200ded17bd144a3c3693e28fb
CPK <samkit157@gmail.com>
2023-03-16 14:34:21 +0800

    fixe iframeMode bugs

--- scripts/SharedConstants.js
+++ scripts/SharedConstants.js
@@ -2,4 +2,5 @@ const assetUrl = "https://viewer.shinycolors.moe";
 const usedFont = "theFont";
 const zhcnFont = "three";
 const translate_master_list = "https://raw.githubusercontent.com/biuuu/ShinyColors/gh-pages/story.json";
-const translate_CSV_url = 'https://raw.githubusercontent.com/biuuu/ShinyColors/gh-pages/data/story/{uid}.csv'
+const translate_CSV_url = 'https://raw.githubusercontent.com/biuuu/ShinyColors/gh-pages/data/story/{uid}.csv';
+const fontTimeout = 3000 // control font loading time
