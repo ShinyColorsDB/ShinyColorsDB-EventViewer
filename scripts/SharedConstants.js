c290077e05477b3cd433562155bf265edd65fc6c
CPK <samkit157@gmail.com>
2023-08-09 10:27:10 +0800

    fix timeoutToClear problems (#38)
    * Cannot find name 'TRACK_INDEXES'.
    
    * fixed timeoutToClear problems

--- scripts/SharedConstants.js
+++ scripts/SharedConstants.js
@@ -1,6 +1,6 @@
 const assetUrl = "https://viewer.shinycolors.moe";
 const usedFont = "theFont";
-const zhcnFont = "three";
+const zhcnFont = "AlimamaShuHeiTi";
 const translate_master_list = "https://raw.githubusercontent.com/biuuu/ShinyColors/gh-pages/story.json";
 const translate_CSV_url = 'https://raw.githubusercontent.com/biuuu/ShinyColors/gh-pages/data/story/{uid}.csv';
 const fontTimeout = 3000; // control font loading time 放弃字体加载超时时间
